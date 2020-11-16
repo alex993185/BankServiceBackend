813c4c0121f667917e5b318fdc4c7d5d42713ca3
Alexander Weigel <alexander.weigel@qognify.com>
2020-11-16 20:51:30 +0100

    add transaction controller

---
+++ BankServiceBackend/Controllers/TransactionController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using BankServiceBackend.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankServiceBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    { 
+        // POST: api/Transaction
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        [Route("deposit")]
+        public async Task<ActionResult> Deposit(Account account, double amount)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("withdraw")]
+        public async Task<ActionResult> Withdraw(Account account, double amount)
+        {
+            return Ok();
+        }
+    }
+}
